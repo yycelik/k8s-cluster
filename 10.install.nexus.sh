27de0afc761be4fd581e412d99ab8fa81ef4507e
YavuzYasinCELIK <yavuzyasincelik@gmail.com>
2023-09-13 22:04:36 +0300

    cert manager, nginx and https configuration for nexus

--- 10.install.nexus.sh
+++ 10.install.nexus.sh
@@ -45,3 +45,69 @@ kubectl create -f volume.yaml
 # get admin pass from node
 kubectl exec -it my-nexus-nexus-repository-manager-5fc8cf4d97-f2wq8 cat /nexus-data/admin.password -n devops-tools
 
+
+
+# cert manager and nginx https configuration
+kubectl apply -f - <<EOF
+---
+apiVersion: cert-manager.io/v1
+kind: ClusterIssuer
+metadata:
+  name: selfsigned-cluster-issuer
+spec:
+  selfSigned: {}
+---
+apiVersion: cert-manager.io/v1
+kind: Certificate
+metadata:
+  name: nexus-ca
+  namespace: cert-manager
+spec:
+  isCA: true
+  commonName: nexus-ca
+  secretName: nexus-ca
+  privateKey:
+    algorithm: ECDSA
+    size: 256
+  dnsNames:
+  - nexus.smart.com
+  issuerRef:
+    name: selfsigned-cluster-issuer
+    kind: ClusterIssuer
+    group: cert-manager.io
+---
+apiVersion: cert-manager.io/v1
+kind: ClusterIssuer
+metadata:
+  name: nexus-cluster-issuer  
+spec:
+  ca:
+    secretName: nexus-ca
+---
+apiVersion: networking.k8s.io/v1
+kind: Ingress
+metadata:
+  annotations:
+    cert-manager.io/cluster-issuer: nexus-cluster-issuer
+  name: nexus-ingress
+  namespace: devops-tools
+spec:
+  ingressClassName: nginx
+  rules:
+  - host: nexus.smart.com
+    http:
+      paths:
+      - pathType: Prefix
+        path: /
+        backend:
+          service:
+            name: my-nexus-nexus-repository-manager
+            port:
+              number: 8081
+  tls:
+  - hosts:
+    - nexus.smart.com
+    secretName: nexus-tls
+
+EOF
+
