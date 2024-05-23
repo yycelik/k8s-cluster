e5c5d019256306faa340e6ebb1c6ec1b3b63d538
YavuzYasinCELIK <yavuzyasincelik@gmail.com>
2024-05-23 12:23:45 +0300

    dns01 configuration

--- 06.03.create.letsencrypt.dns01.certificate.sh
+++ 06.03.create.letsencrypt.dns01.certificate.sh
@@ -36,3 +36,38 @@ spec:
   - "*.s3t.co"
   - "*.nexus.s3t.co"
 EOF
+
+
+
+# put that name to dns server with {Key}
+# _acme-challenge.<YOUR_DOMAIN> TEXT 
+kubectl describe challenge
+
+#kubectl describe challenge
+#Name:         s3t-wildcard-cert-prod-3-4058508564-1170114524
+#Namespace:    default
+#Labels:       <none>
+#Annotations:  <none>
+#API Version:  acme.cert-manager.io/v1
+#Kind:         Challenge
+#Metadata:
+#  Creation Timestamp:  2024-05-23T08:25:55Z
+#  Finalizers:
+#    finalizer.acme.cert-manager.io
+#  Generation:  1
+#  Owner References:
+#    API Version:           acme.cert-manager.io/v1
+#    Block Owner Deletion:  true
+#    Controller:            true
+#    Kind:                  Order
+#    Name:                  s3t-wildcard-cert-prod-3-4058508564
+#    UID:                   176dcb30-0a5e-430d-9fda-0104bd2eeabb
+#  Resource Version:        5829614
+#  UID:                     d448c8c4-a13c-4435-8627-2f43045d2fe7
+#Spec:
+#  Authorization URL:  https://acme-v02.api.letsencrypt.org/acme/authz-v3/354335565952
+#  Dns Name:           s3t.co
+#  Issuer Ref:
+#    Kind:  ClusterIssuer
+#    Name:  letsencrypt-prod
+#  Key:     dxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxM
